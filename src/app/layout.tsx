3e76404d049f12170855c39c0e059248bda2894c
Ito <ito30@users.noreply.github.com>
2024-07-03 18:39:53 +0700

    feat: add BOSS LELE title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>BOSS LELE</title>
       <body className={inter.className}>
         <Layout style={{ minHeight: "100vh" }}>
           <BurgerMenu />
